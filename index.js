4ad88f2f96c98906be97f82e2082b0a14d827c1a
Abhinav <ajagekarajagekar@gmail.com>
2023-02-24 17:58:43 +0530

    installed ejs and created view for home

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+// use express router
 app.use('/', require('./routes'));
 
+
+// set up the view engine
+app.set('view engine', 'ejs');
+app.set('views','./views');
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
 app.listen(port, function(err){
     if(err){
