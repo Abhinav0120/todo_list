a55c6ba753e18a01a8ff613188fe1ffff41e67ca
Abhinav <ajagekarajagekar@gmail.com>
2023-02-25 00:28:30 +0530

    created the task Schema and stored task data in the database

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const db = require('./config/mongoose');
+const Task = require('./models/task');
+const Contact = require('./models/task');
+
 const app = express();
 const port = 8000;
 
@@ -14,6 +17,24 @@ app.set('view engine', 'ejs');
 app.set('views','./views');
 app.use(express.urlencoded());
 app.use(express.static('assets'));
+
+app.post('/create-task', function(req, res){
+
+    Task.create({
+        description: req.body.description,
+        category: req.body.category,
+        duedate: req.body.duedate
+    }, function(err, newTask){
+        if(err){
+            console.log('error in creating a task');
+            return;
+        }
+
+        console.log('*****', newTask);
+        return res.redirect('back');
+    });
+
+});
 
 app.listen(port, function(err){
     if(err){
